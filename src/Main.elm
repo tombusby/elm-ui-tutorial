efd8b0fb85f018c6c3ee59ab5dec1037a9e97f90
Tom Busby <tom@busby.ninja>
2020-02-24 22:57:18 +0100

    Initial Commit: Basic project skeleton

---
+++ src/Main.elm
@@ -0,0 +1,34 @@
+module Main exposing (main)
+
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
+
+channelPanel : Element msg
+channelPanel =
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , Background.color <| rgb255 92 99 118
+        , Font.color <| rgb255 255 255 255
+        ]
+        [ text "channels" ]
+
+
+chatPanel : Element msg
+chatPanel =
+    column [ height fill, width <| fillPortion 5 ]
+        [ text "chat" ]
+
+
+main : Html msg
+main =
+    layout [] <|
+        row [ height fill, width fill ]
+            [ channelPanel
+            , chatPanel
+            ]
