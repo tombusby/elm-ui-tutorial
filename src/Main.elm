3e5276be07366b456ad4b2f4f8cd8115adb8ab3e
Tom Busby <tom@busby.ninja>
2020-02-25 16:55:09 +0100

    Add more elements to the layout

--- src/Main.elm
+++ src/Main.elm
@@ -8,27 +8,95 @@ import Element.Font as Font
 import Element.Input as Input
 import Html exposing (Html)
 
-channelPanel : Element msg
-channelPanel =
+
+channelPanel : List String -> String -> Element msg
+channelPanel channels activeChannel =
+    let
+        activeChannelAttrs =
+            [ Background.color <| rgb255 117 179 201, Font.bold ]
+
+        channelAttrs =
+            [ paddingXY 15 5, width fill ]
+
+        channelEl channel =
+            el
+                (if channel == activeChannel then
+                    activeChannelAttrs ++ channelAttrs
+
+                 else
+                    channelAttrs
+                )
+            <|
+                text ("# " ++ channel)
+    in
     column
         [ height fill
         , width <| fillPortion 1
+        , paddingXY 0 10
         , Background.color <| rgb255 92 99 118
         , Font.color <| rgb255 255 255 255
         ]
-        [ text "channels" ]
+    <|
+        List.map channelEl channels
 
 
-chatPanel : Element msg
-chatPanel =
+chatPanel : String -> Element msg
+chatPanel channel =
+    let
+        header =
+            row
+                [ width fill
+                , paddingXY 20 5
+                , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+                , Border.color <| rgb255 200 200 200
+                ]
+                [ el [] <| text ("#" ++ channel)
+                , Input.button
+                    [ padding 5
+                    , alignRight
+                    , Border.width 1
+                    , Border.rounded 3
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    { onPress = Nothing
+                    , label = text "Search"
+                    }
+                ]
+
+        messagePanel =
+            column [] []
+
+        footer =
+            el [ alignBottom, padding 20, width fill ] <|
+                row
+                    [ spacingXY 2 0
+                    , width fill
+                    , Border.width 2
+                    , Border.rounded 4
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    [ el
+                        [ padding 5
+                        , Border.widthEach { right = 2, left = 0, top = 0, bottom = 0 }
+                        , Border.color <| rgb255 200 200 200
+                        , mouseOver [ Background.color <| rgb255 86 182 139 ]
+                        ]
+                      <|
+                        text "+"
+                    , el [ Background.color <| rgb255 255 255 255 ] none
+                    ]
+    in
     column [ height fill, width <| fillPortion 5 ]
-        [ text "chat" ]
+        [ header
+        , messagePanel
+        , footer
+        ]
 
 
 main : Html msg
 main =
     layout [] <|
         row [ height fill, width fill ]
-            [ channelPanel
-            , chatPanel
+            [ channelPanel [ "elm", "elm-ui", "elm-format" ] ""
+            , chatPanel "test"
             ]
